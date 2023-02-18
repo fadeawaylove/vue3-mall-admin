f5e401b44bfbc733b5ea00a93b809e6213ccbba5
邓润庭 <1032939141@qq.com>
2023-02-18 16:46:55 +0800

    feat:初始化-引入windicss并简单使用

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,18 @@
 </script>
 
 <template>
-  <el-row class="mb-4">
+  <button class="btn">按钮</button>
+<!-- <el-row class="mb-4">
     <el-button>Default</el-button>
     <el-button type="primary">Primary</el-button>
     <el-button type="success">Success</el-button>
     <el-button type="info">Info</el-button>
     <el-button type="warning">Warning</el-button>
     <el-button type="danger">Danger</el-button>
-  </el-row>
-</template>
+  </el-row> --></template>
 
 <style scoped>
-
+.btn {
+  @apply bg-purple-500 text-indigo-50 px-4 py-2 rounded-full transition-all duration-500 hover: (bg-purple-900) focus:(ring-8 ring-purple-900);
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import './style.css'
 import App from './App.vue'
 
 const app = createApp(App)
 
 app.use(ElementPlus)
+
+import 'virtual:windi.css'
+
 app.mount('#app')
