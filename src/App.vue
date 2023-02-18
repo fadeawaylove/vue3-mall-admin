4bfdbdb9ba2f950b326ca450d41d1397f40f54b4
邓润庭 <1032939141@qq.com>
2023-02-18 17:28:05 +0800

    feat:404页面

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <button class="btn">按钮</button>
+  <router-view></router-view>
 <!-- <el-row class="mb-4">
     <el-button>Default</el-button>
     <el-button type="primary">Primary</el-button>
@@ -13,7 +13,4 @@
   </el-row> --></template>
 
 <style scoped>
-.btn {
-  @apply bg-purple-500 text-indigo-50 px-4 py-2 rounded-full transition-all duration-500 hover: (bg-purple-900) focus:(ring-8 ring-purple-900);
-}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,24 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
+import Index from '~/pages/index.vue'
+import About from '~/pages/about.vue'
+import NotFound from '~/pages/404.vue'
 
 
-const routes = [];
+const routes = [
+    {
+        path: "/",
+        component: Index
+    },
+    {
+        path: "/about",
+        component: About
+    },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "NotFound",
+        component: NotFound
+    }
+];
 
 
 
