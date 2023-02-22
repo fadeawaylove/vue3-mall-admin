1ae66c178a51e98e8fe2462cc4f0379b72300ae5
邓润庭 <1032939141@qq.com>
2023-02-22 20:13:26 +0800

    feat：动态添加路由

--- src/permission.js
+++ src/permission.js
@@ -1,4 +1,4 @@
-import router from '~/router/index'
+import { router, addRoutes } from '~/router'
 import { getToken } from '~/utils/auth'
 import { toast, showFullLoading, hideFullLoading } from '~/utils/notify'
 import store from './store'
@@ -9,7 +9,7 @@ router.beforeEach(async (to, from, next) => {
     showFullLoading()
 
     const token = getToken()
-    console.log(to, from, token)
+    // console.log(to, from, token)
 
     if (!token && to.path != "/login") {
         toast("请先登录", "error")
@@ -19,14 +19,16 @@ router.beforeEach(async (to, from, next) => {
         toast("请勿重复登录", "error")
         return next({ path: from.path ? from.path : "/" })
     }
-
+    let hasNewRoutes = false
     if (token) {
-        await store.dispatch('getinfo')
+        let { menus } = await store.dispatch('getinfo')
+        // 动态添加路由
+        hasNewRoutes = addRoutes(menus)
     }
 
     let title = (to.meta.title ? to.meta.title : "") + "-呱呱呱"
     document.title = title
-    next();
+    hasNewRoutes ? next(to.fullPath) : next()
 })
 
 
--- src/router/index.js
+++ src/router/index.js
@@ -7,33 +7,14 @@ import Admin from '~/layouts/admin.vue';
 import GoodList from '~/pages/goods/list.vue'
 import CategoryList from '~/pages/category/list.vue'
 
+
+// 默认路由，所有用户共享
 const routes = [
     {
         path: "/",
+        name: "admin",
         component: Admin,
-        children: [
-            {
-                path: "/",
-                component: Index,
-                meta: {
-                    title: "后台首页"
-                }
-            },
-            {
-                path: "/goods/list",
-                component: GoodList,
-                meta: {
-                    title: "商品管理"
-                }
-            },
-            {
-                path: "/category/list",
-                component: CategoryList,
-                meta: {
-                    title: "分类列表"
-                }
-            }
-        ]
+        children: []
 
     },
     {
@@ -58,14 +39,60 @@ const routes = [
             title: "未知页面"
         }
     }
-];
+]
+
+// 定义动态路由
+const dynamicRoute = [
+    {
+        path: "/",
+        name: "/",
+        component: Index,
+        meta: {
+            title: "后台首页"
+        }
+    },
+    {
+        path: "/goods/list",
+        name: "/goods/list",
+        component: GoodList,
+        meta: {
+            title: "商品管理"
+        }
+    },
+    {
+        path: "/category/list",
+        name: "/category/list",
+        component: CategoryList,
+        meta: {
+            title: "分类列表"
+        }
+    }
+]
 
 
-
-const router = createRouter({
+export const router = createRouter({
     history: createWebHashHistory(),
     routes: routes
 
 })
 
-export default router;
+// 动态添加路由的方法
+export function addRoutes(menus) {
+    // 是否有新的路由
+    let hasNewRoutes = false
+    const find = (arr) => {
+        arr.forEach(element => {
+            let item = dynamicRoute.find(o => o.path == element.frontpath)
+            if (item && !router.hasRoute(item.path)) {
+                router.addRoute("admin", item)
+                hasNewRoutes = true
+            }
+            if (element.child && element.child.length > 0) {
+                find(element.child)
+            }
+        });
+    }
+    find(menus)
+    // console.log(router.getRoutes())
+    return hasNewRoutes
+}
