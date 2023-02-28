427e24c0874962af8aa18431c127f53deed987d4
dengrunting <1032939141@qq.com>
2023-02-28 16:04:08 +0800

    temp:暂存

---
+++ src/components/IndexCard.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+    <el-card shadow="never" :body-style="{ padding: '20px' }">
+        <template #header>
+        <div>
+            <span><!-- card title --></span>
+        </div>
+        </template>
+        <!-- card body -->
+    </el-card>
+    
+
+
+</div>
+</template>
+    
